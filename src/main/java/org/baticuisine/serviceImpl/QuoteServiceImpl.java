c8168bb2061c904703b8ddec64e03d1653ac212f
Douae sb <douae.sb411@gmail.com>
2024-11-11 14:08:29 +0100

    git directory

--- src/main/java/org/baticuisine/serviceImpl/QuoteServiceImpl.java
+++ src/main/java/org/baticuisine/serviceImpl/QuoteServiceImpl.java
@@ -4,6 +4,9 @@ import org.baticuisine.entities.Quote;
 import org.baticuisine.repositoryImpl.QuoteRepositoryImpl;
 import org.baticuisine.service.QuoteService;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class QuoteServiceImpl implements QuoteService {
 
     private QuoteRepositoryImpl quoteRepository;
@@ -15,5 +18,9 @@ public class QuoteServiceImpl implements QuoteService {
     @Override
     public void addQuote(Quote quote){
         quoteRepository.addQuote(quote);
+    }
+
+    public List<String> getNoms(List<Quote> quotes){
+        return quotes.stream().map(devis->devis.getProject().getClient().getName()).collect(Collectors.toList());
     }
 }
